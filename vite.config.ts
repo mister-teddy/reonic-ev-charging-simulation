577c5db91a22bda21a805b242a5c7562047fb789
Nguyen Hong Phat <hongphat.js@gmail.com>
2025-10-29 00:22:04 +0100

    deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/reonic-ev-charging-simulation/",
   plugins: [
     react({
       babel: {
